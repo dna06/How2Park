58717b72b99a8ee8bdfd6e33c620e12d11a4d7c7
DerPanter <62297495+DerPanter@users.noreply.github.com>
2020-05-19 00:36:09 +0200

    Triggers

---
+++ Assets/Trigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggerz : MonoBehaviour
+{
+
+    private HashSet<Collider> m_colliders = new HashSet<Collider>();
+
+
+    void OnTriggerEnter(Collider DZone)
+    {
+
+        m_colliders.Add(DZone);
+
+    }
+
+
+    void OnTriggerExit(Collider Dzone)
+    {
+
+        m_colliders.Remove(Dzone);
+
+    }
+
+    void Update()
+    {
+
+
+        if (m_colliders.Count == 4)
+        {
+            Debug.Log("You Did It!! :D");
+        }
+
+        else
+        {
+            Debug.Log("Waiting or it...");
+        }
+
+        Debug.Log(m_colliders.Count);
+    }
+}

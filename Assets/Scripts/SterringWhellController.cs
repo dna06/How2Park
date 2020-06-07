d168a90d5d85a038bb301e048f298e6dc464938d
dna06 <56910040+dna06@users.noreply.github.com>
2020-06-07 19:44:05 +0200

    newsdk_unityassetsore

---
+++ Assets/Scripts/SterringWhellController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringWheelController : MonoBehaviour
+{
+    private static SteeringWheelController instance = null;
+
+    public static SteeringWheelController GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameObject("SteeringWheelCtrl").AddComponent<SteeringWheelController>();
+            //1, two kinds of initialization, see the requirements can be used.
+            Debug.Log("SteeringInit:" + LogitechGSDK.LogiLedInit());
+            Debug.Log("SteeringInit2:" + LogitechGSDK.LogiSteeringInitialize(false));
+        }
+        return instance;
+    }
+    //2 before exiting
+    void OnApplicationQuit()
+    {
+        Debug.Log("SteeringShutdown:" + LogitechGSDK.LogiSteeringShutdown());
+    }
+}
+

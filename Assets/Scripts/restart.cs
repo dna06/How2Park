29fbb16035ce8dadf54df45a5611e183aaad78ab
dna06 <56910040+dna06@users.noreply.github.com>
2020-06-08 23:19:57 +0200

    change_scipts

---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class restart : MonoBehaviour {
+
+void Update ()
+{
+if(Input.GetKeyDown(KeyCode.R))
+{
+            SceneManager.LoadScene("test");
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+           {
+            Debug.Log("aus"); 
+            Application.Quit();
+
+            
+        }
+    }
+}
+ 
